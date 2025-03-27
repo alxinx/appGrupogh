2edf5d79bd702f75bed402e1ae9fbd1d913b14e0
System Administrator <root@192.168.1.33>
2025-03-27 15:31:26 -0500

    se modifico el aarchivo config/db.js y se repararon los enlaces de regisster.

--- config/bd.js
+++ config/bd.js
@@ -1,8 +1,8 @@
 import { Sequelize } from "sequelize";
 import dotenv from "dotenv";
-const db = new Sequelize(DB_NAME,DB_USER, DB_PASS, {
-    host : DB_HOST,
-    port : DB_PORT,
+const db = new Sequelize(process.env.DB_NAME,process.env.DB_USER, process.env.DB_PASS, {
+    host : process.env.DB_HOST,
+    port : process.env.DB_PORT,
     dialect : "mysql",
     define : {
         timestamps : true,
@@ -14,7 +14,7 @@ const db = new Sequelize(DB_NAME,DB_USER, DB_PASS, {
         idle : 10000,
     },
     logging : false,
-    minifyAliases : true
+    minifyAliases : true,
     
 })
 
